47000900a86f8bb2f896f5749b918fa8de515288
UltraInfinity <144112622+UltraInfinity@users.noreply.github.com>
2023-10-10 05:55:42 +0100

    commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,7 +21,7 @@ function Projects() {
             <ProjectCard
               isBlog={false}
               title="Library Loan Analysis"
-              description="Add description"
+              description="In this university assignment, I designed and implemented a Python program to analyze library loan data. The project involved processing data from two sources: book information and loan records. Using Python, I calculated the popularity of books based on the number of days borrowed and generated a report highlighting the most borrowed books. Additionally, I analyzed book genres, identified trends, and created a report on genre popularity. Lastly, I calculated loan statistics, including total loan days, average loan days, and the percentage of books returned late. This project showcases my data analysis, Python programming, and data visualization skills, providing valuable insights into library usage patterns."
               ghLink="https://github.com/UltraInfinity"
             />
           </Col>
@@ -40,7 +40,7 @@ function Projects() {
               isBlog={false}
               title="Data Visualization - GDP and Life Expectancy"
               description="This project showcases my expertise in data visualization. I analyzed data on GDP and life expectancy, transforming raw information into meaningful visual representations. Through graphs and charts, I communicated complex relationships and patterns, highlighting my ability to convey insights effectively to both technical and non-technical audiences."
-              ghLink="https://github.com/soumyajit4419/Editor.io"             
+              ghLink="https://github.com/UltraInfinity"             
             />
           </Col>
 
@@ -57,9 +57,9 @@ function Projects() {
             <ProjectCard
               /*imgPath={}*/
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Group Project - OKCupid Date-A-Scientist"
+              description="Collaboration is an essential skill in the world of data science. In this group project, I worked with a team to leverage machine learning algorithms to analyze OKCupid data. This experience not only highlighted my ability to collaborate effectively but also reinforced my knowledge of machine learning and predictive modeling."
+              ghLink="https://github.com/UltraInfinity"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
@@ -67,10 +67,9 @@ function Projects() {
           <Col md={4} className="project-card">
             <ProjectCard
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="OkCupid Date-A-Scientist Machine Learning"
+              description="This machine learning project showcases my capabilities in building, training, and testing predictive models. Using data from OKCupid, I applied machine learning techniques to make predictions and draw insights. This project reflects my proficiency in machine learning, a critical skill for data scientists."
+              ghLink="https://github.com/UltraInfinity"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
