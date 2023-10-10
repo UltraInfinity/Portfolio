560c7ae19afc7c1c82b4822a15cf6ec2f4e9ba6c
UltraInfinity <144112622+UltraInfinity@users.noreply.github.com>
2023-10-10 05:49:24 +0100

    commit

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,32 +7,29 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      {/* No Card.Img element */}
-      <Card.Body>
+      <Card.Body style={{ display: "flex", flexDirection: "column" }}>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
+        <Card.Text style={{ textAlign: "justify", flex: 1 }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
 
         {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+        <div style={{ marginTop: "auto" }}>
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              style={{ marginBottom: "10px" }}
+            >
+              <CgWebsite /> &nbsp; Demo
+            </Button>
+          )}
 
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp; {props.isBlog ? "Blog" : "GitHub"}
           </Button>
-        )}
+        </div>
       </Card.Body>
     </Card>
   );
