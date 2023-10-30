e6e7d4782acd271bbaa7164a05a3db41acf8b5e6
UltraInfinity <144112622+UltraInfinity@users.noreply.github.com>
2023-10-30 00:04:37 +0000

    commit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -35,18 +35,6 @@ function AboutCard() {
           </p>
           <footer className="blockquote-footer">Ahmed Najaf</footer>
         </blockquote>
-        <div itemscope itemtype='http://schema.org/Person' className='fiverr-seller-widget' style={{display: 'inline-block'}}>
-    <a itemprop='url' href='https://www.fiverr.com/ahmednajaf2' rel="nofollow" target="_blank" style={{display: 'inline-block'}}>
-        <div className='fiverr-seller-content' id='fiverr-seller-widget-content-2600f161-6fbd-4f29-9a55-c9acc61d40d0' itemprop='contentURL' style={{display: 'none'}}></div>
-        <div id='fiverr-widget-seller-data' style={{display: 'none'}}>
-            <div itemprop='name'>ahmednajaf2</div>
-            <div itemscope itemtype='http://schema.org/Organization'><span itemprop='name'>Fiverr</span></div>
-            <div itemprop='jobtitle'>Seller</div>
-            <div itemprop='description'>Hi! I'm a Computer Science student with a knack for web design and Python development...</div>
-        </div>
-    </a>
-</div>
-
       </Card.Body>
     </Card>
   );
